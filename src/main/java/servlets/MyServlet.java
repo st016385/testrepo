2564d8bd78c46ca40c90b08c88dbd898d04b6e8e
st016385 <artur.2401@ya.ru>
2015-08-13 17:04:38 +0300

    initial version

---
+++ src/main/java/servlets/MyServlet.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+/**
+ * Created by jessie on 12.08.15.
+ */
+@WebServlet("/s")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("Hello Word");
+    }
+}
